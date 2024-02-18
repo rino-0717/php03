7736b39d335f306ff4104f275f7bc9105cc3b0a5
rino-0717 <rino.justinb@gmail.com>
2024-02-18 11:47:40 +0900

    コミットメッセージ

--- src/php03/index.php
+++ src/php03/index.php
@@ -17,5 +17,28 @@
             </a>
         </div>
     </header>
+    <main>
+    <div class="search-form__content">
+   <!-- // ここにラジオボタンを作成します。 -->
+        <div class="search-form__heading">
+            <h2>日本と世界の時間を比較</h2>
+        </div>
+    </div>
+        <form class="search-form" action="result.php" method="get">
+        <div class="search-form__item">
+            <select class="search-form__item-select" name="city">
+            <option value="シドニー">シドニー</option>
+            <option value="上海">上海</option>
+            <option value="モスクワ">モスクワ</option>
+            <option value="ロンドン">ロンドン</option>
+            <option value="ヨハネスブルグ">ヨハネスブルグ</option>
+            <option value="ニューヨーク">ニューヨーク</option>
+            </select>
+        </div>
+        <div class="search-form__button">
+            <button class="search-form__button-submit" type="submit">検索<button>
+        </div>
+        </form>
+    </main>
 </body>
 </html>
